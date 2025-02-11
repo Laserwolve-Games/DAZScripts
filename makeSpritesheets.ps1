347e7e65f8f45d949d2668c9303e500e5a9136ad
Laserwolve <AndrewRogers@LaserwolveGames.com>
2025-02-11 11:00:09 -0400

    Changed file extensions

--- makeSpritesheets.ps1
+++ makeSpritesheets.ps1
@@ -6,7 +6,7 @@ foreach ($dir in $subDirs) {
 
     $folderName = $dir.Name
 
-    TexturePacker --config "C:/assets/DAZScripts/settings.tps" --sheet "$outputDir\$folderName.webp" --data "$outputDir\$folderName.json" $dir.FullName
+    TexturePacker "C:/assets/DAZScripts/settings.tps" --sheet "$outputDir\$folderName.webp" --data "$outputDir\$folderName.json" $dir.FullName
 
     Remove-Item -Recurse -Force $dir.FullName
 }
