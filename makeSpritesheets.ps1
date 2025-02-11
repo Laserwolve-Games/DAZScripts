02a6c3104eb243b0de1c5c0babea235d62755e7f
Laserwolve <AndrewRogers@LaserwolveGames.com>
2025-02-11 10:42:37 -0400

    Added spritesheeting to flow

---
+++ makeSpritesheets.ps1
@@ -0,0 +1,13 @@
+
+$outputDir = "C:\output"
+
+$subDirs = Get-ChildItem -Path $outputDir -Directory
+
+foreach ($dir in $subDirs) {
+
+    $folderName = $dir.Name
+
+    TexturePacker --config "C:/assets/DAZScripts/settings.tps" --sheet "$outputDir\$folderName.webp" --data "$outputDir\$folderName.json" $dir.FullName
+    
+    Remove-Item -Recurse -Force $dir.FullName
+}
