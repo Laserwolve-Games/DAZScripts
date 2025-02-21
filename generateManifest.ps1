ecb5fd67eb4d48b8e5cbece1d8626d4e1cf5d804
Laserwolve <AndrewRogers@LaserwolveGames.com>
2025-02-21 16:34:21 -0400

    rough-in for a manifest generator

---
+++ generateManifest.ps1
@@ -0,0 +1,22 @@
+param (
+    [string]$targetDirectory = "C:\PlainsOfShinar\spritesheets"
+)
+
+$items = Get-ChildItem -Path $targetDirectory -Recurse
+
+$outputData = @() # Initialize as an array
+
+foreach ($item in $items) {
+    $outputData += [pscustomobject]@{
+        Name = $item.Name
+        FullName = $item.FullName
+        ItemType = if ($item.PSIsContainer) { "Directory" } else { "File" }
+    }
+}
+
+$outputJson = $outputData | ConvertTo-Json -Depth 10
+$outputJsonPath = Join-Path -Path $targetDirectory -ChildPath "manifest.json"
+
+$outputJson | Set-Content -Path $outputJsonPath
+
+Write-Output "JSON file created at $outputJsonPath"
--- makeSpritesheets.ps1
+++ makeSpritesheets.ps1
@@ -18,10 +18,18 @@ foreach ($dir in $subDirs) {
 
     Write-Output "Processing folder: $folderName"
 
-    # Underscores instead of dashes in the file name weren't working
-    TexturePacker "settings.tps" --sheet "$outputDir\$folderName\$folderName-{n}.webp" --data "$outputDir\$folderName\$folderName-{n}.json" $fullName
+    TexturePacker "settings.tps" --sheet "$outputDir\$folderName.webp" --data "$outputDir\$folderName.json" $fullName
 
     if ((Test-Path "$outputDir\$folderName.webp") -and (Test-Path "$outputDir\$folderName.json")) {
         Remove-Item -Recurse -Force $fullName
     }
+}
+
+# Execute the generateManifest.ps1 script
+$manifestScriptPath = "C:\assets\DAZScripts\generateManifest.ps1"
+if (Test-Path $manifestScriptPath) {
+    Write-Output "Executing generateManifest.ps1..."
+    & $manifestScriptPath
+} else {
+    Write-Output "generateManifest.ps1 not found at $manifestScriptPath"
 }
