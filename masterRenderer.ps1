bf02a9acd2906883ebbfb6398616d7eb311bf3d7
Laserwolve <AndrewRogers@LaserwolveGames.com>
2025-03-28 10:18:51 -0400

    Update masterRenderer.ps1

--- masterRenderer.ps1
+++ masterRenderer.ps1
@@ -1,1 +1,6 @@
-Start-Process -FilePath "C:\Program Files\DAZ 3D\DAZStudio4\DAZStudio.exe" -ArgumentList "-instanceName", "#", "-scriptArg", "1", "-logSize", "500000000", "-noPrompt"
+Start-Process -FilePath "C:\Program Files\DAZ 3D\DAZStudio4\DAZStudio.exe" `
+    -ArgumentList "-scriptArg", "1", ` # How many instances will be launched
+                  "-instanceName", "#", ` # Incrementally number the instances
+                  "-logSize", "500000000", ` # 500 MB
+                  "-noPrompt", ` # https://github.com/Laserwolve-Games/DAZScripts/discussions/1
+                  "C:\Users\Andre\OneDrive\repositories\DAZScripts\masterRenderer.dsa" # The script to execute on launch
